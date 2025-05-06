e47bdf64452128452847ec9e9d109c8d92303557
penscola <penscolashackletonfelix@gmail.com>
2025-05-06 23:56:19 +0300

    closure

---
+++ project/closure/src/main.rs
@@ -0,0 +1,6 @@
+pub mod test_closure_file;
+
+fn main() {
+    println!("Hello world");
+    test_closure_file::closure::test_closure();
+}
---
+++ project/closure/src/test_closure_file/closure.rs
@@ -0,0 +1,4 @@
+pub fn test_closure() {
+    let add = |i: i8, y: i8| x+y;
+    add(1, 8);
+}
