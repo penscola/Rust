63b20ed1f678429bd88f5eb5211db2dfea08a59f
penscola <penscolashackletonfelix@gmail.com>
2025-07-28 22:53:27 +0300

    :boom - changes

--- src/math/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod math_functions;
-
-
--- src/math/src/main.rs
+++
@@ -1,7 +0,0 @@
-extern crate math;
-fn main() {
-    println!("Sum: {}", math::math_functions::functions::sum(5, 2));
-    println!("Subtraction: {}", math::math_functions::functions::sub(5, 2));
-    println!("Multiplication: {}", math::math_functions::functions::mul(5, 2));
-    println!("Division: {}", math::math_functions::functions::div(5, 2));
-}
